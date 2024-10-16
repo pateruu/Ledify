2e0abceb19f352147fa882aca34d2c60a93ecc4d
Kevin Patel <kxvin.pxtel1@gmail.com>
2024-10-15 23:31:51 -0400

    Cleaned up led_control.cpp file. Added necessary comments and cleaned up spacing

--- LEDify/src/led_control.cpp
+++ LEDify/src/led_control.cpp
@@ -6,6 +6,7 @@
 //Instance of Adafruit_NeoPixel
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, COLOUR_ORDER + NEO_KHZ800);
 
+//Define Variables
 uint8_t currentR;
 uint8_t currentG;
 uint8_t currentB;
@@ -17,31 +18,31 @@ bool isOn;
 void initializeLEDS(){
     strip.begin();
 
-    setStripColour(0, 0, 0, 0); //turn off all LEDS
+    //turn off all LEDS
+    setStripColour(0, 0, 0, 0);
     strip.setBrightness(150);
     delay(1000);
 
+    //LED Verification Check
     for(int i = 0; i < 3; i++){
-        setLEDColour(0, 150, 0, 0, 0); //turn on red
+        setLEDColour(0, 150, 0, 0, 0); 
         delay(500);
-        
-        setLEDColour(0, 0, 0, 0, 0); //turn off
+        setLEDColour(0, 0, 0, 0, 0);
         delay(500);
     }
-
-    setLEDColour(0, 0, 150, 0, 0); //turn on green
-    
+    setLEDColour(0, 0, 150, 0, 0);
     delay(500);
 }
 
 //Set the whole strip colour
 void setStripColour(uint8_t r, uint8_t g, uint8_t b, uint8_t w){
+  //Set all LEDs to given colour
   for(int i = 0; i < NUM_LEDS; i++) {
     strip.setPixelColor(i, strip.Color(r, g, b, w));
   }
   strip.show();
 
-  //Store current colour values.
+  //Update current colour values.
   currentR = r;
   currentG = g;
   currentB = b;
@@ -50,17 +51,17 @@ void setStripColour(uint8_t r, uint8_t g, uint8_t b, uint8_t w){
 
 //Set an individual LED colour
 void setLEDColour(int n, uint8_t r, uint8_t g, uint8_t b, uint8_t w){
-
+    //If given LED is out of bounds
     if( n > NUM_LEDS || n < 0){
         Serial.println("LED index out of bounds.");
         return;
-    } else{
+    } else{ //Set the pixel colour.
         strip.setPixelColor(n, strip.Color(r, g, b, w));
         strip.show();
     }
 }
 
-//Function to turn off the LEDs
+//Function to turn off all LEDs
 void turnOffLEDS(){
     for(int i=0; i < NUM_LEDS; i++){
         strip.setPixelColor(i, strip.Color(0, 0, 0, 0));
@@ -68,17 +69,15 @@ void turnOffLEDS(){
     strip.show();
 }
 
-//Toggle LEDs 
+//Toggle LEDs On/Off
 void togglePowerButton(){
-    if(isOn){
+    if(isOn){ //if on then turn off and set isOn to false
         Serial.println("Turning off LEDs...");
-        turnOffLEDS(); //Turn off the leds
-        isOn = false; //Toggle isOn value.
-    } else{
+        turnOffLEDS();
+        isOn = false;   
+    } else{ //If off then turn on and set isOn to true
         Serial.println("Turning on LEDs...");
-        setStripColour(currentR, currentG, currentB, currentW); //Restore last stored values.
-        isOn = true; //Toggle isOn value.
+        setStripColour(currentR, currentG, currentB, currentW);
+        isOn = true;
     }
 }
-
-
