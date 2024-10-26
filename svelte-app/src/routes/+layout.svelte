15a844627247841fde40d9e449accab0c8d197b5
Kevin Patel <kxvin.pxtel1@gmail.com>
2024-10-26 03:54:43 -0400

    Made general layout page to have background style and navbar so that it is synced throughout all pages.

---
+++ svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script>
+
+</script>
+
+<main>
+  <ul class="navbar">
+    <li><a href='/info'>Info</a></li>
+    <li><a href='/setup'>Setup</a></li>
+    <li><a href='/main-ui'>Main UI</a></li>
+    <li><a href='/colour-buttons'>Colour Buttons</a></li>
+    <li><a href='/animations'>Animations</a></li>
+  </ul>
+
+    <!-- This is where the content of individual pages will go -->
+    <slot></slot>
+</main>
+
+<style>
+  :global(body){
+    background-color: #2c2c2c;
+    font-family: Arial, Helvetica, sans-serif;
+    color: white;
+    margin: 0;
+    }
+
+  .navbar{
+    display: flex;
+    justify-content: space-around;
+    background-color: #333;
+    padding: 1rem;
+    list-style-type: none;
+  }
+    
+  .navbar li{
+      margin: 0 10px;
+  }
+
+  .navbar li a {
+    color: white;
+    text-decoration: none;
+  }
+
+  .navbar li a:hover {
+    color: #ccc;
+  }
+</style>
+
