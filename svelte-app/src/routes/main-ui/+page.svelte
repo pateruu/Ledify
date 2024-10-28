5df86abebe29d6220dd2b827ad904d6c5155f691
Kevin Patel <kxvin.pxtel1@gmail.com>
2024-10-28 09:47:45 -0400

    Added html to the main-ui svelte page.

--- svelte-app/src/routes/main-ui/+page.svelte
+++ svelte-app/src/routes/main-ui/+page.svelte
@@ -1,7 +1,45 @@
+<script>
+
+</script>
+
 <main>
-    <h1>This is Main UI Page</h1>
+    <div class="container">
+        <h1>LEDify Control Panel</h1>
+
+        <div class="power-control">
+            <button id="power-button">Power</button>
+        </div>
+
+        <div class="control-panel" id="defaultPicker">
+            <label for="color-picker">Pick a Color:</label>
+        </div>
+
+        <div class="brightness-level">
+            <label for="brightness-slider">Brightness:</label>
+            <input type="range" id="brightness-slider" min="0" max="255" value="128">
+        </div>
+
+        <div class="preset-colours">
+            <h2>Preset Colors</h2>
+            <button class="preset-colour" data-colour="#ff0000" style="background-color: #ff0000;">Red</button>
+            <button class="preset-colour" data-colour="#00ff00" style="background-color: #00ff00;">Green</button>
+            <button class="preset-colour" data-colour="#0000ff" style="background-color: #0000ff;">Blue</button>
+            <button class="preset-colour" data-colour="#ffffff" style="background-color: #ffffff;">White</button>
+            <button class="preset-colour" data-colour="#800080" style="background-color: #800080;">Purple</button>
+            <button class="preset-colour" data-colour="#ffff00" style="background-color: #ffff00;">Yellow</button>
+            <button class="preset-colour" data-colour="#ffa500" style="background-color: #ffa500;">Orange</button>
+            <button class="preset-colour" data-colour="#ff1493" style="background-color: #ff1493;">Pink</button>
+            <button class="preset-colour" data-colour="#00ffff" style="background-color: #00ffff;">Cyan</button>
+            <button class="preset-colour" data-colour="#ff69b4" style="background-color: #ff69b4;">Hot Pink</button>
+            <button class="preset-colour" data-colour="#008000" style="background-color: #008000;">Dark Green</button>
+            <button class="preset-colour" data-colour="#ffd700" style="background-color: #ffd700;">Gold</button>
+
+            
+        </div>
+    </div>
+    <script src="/script.js"></script>
 </main>
     
 <style>
-
+    
 </style>
