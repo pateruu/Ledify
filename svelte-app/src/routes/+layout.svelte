76f6bdf48e0874c14b76e9bdc5a8ed7cb3c7795b
Kevin Patel <kxvin.pxtel1@gmail.com>
2024-10-30 11:03:49 -0400

    Offically got flowbite Svelte working with project. No errors found. Downloaded all required libraries.

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,49 +1,48 @@
 <script>
-  import '../app.css';
+	import '../app.css';
 </script>
 
 <main>
-  <!-- Global Navbar -->
-  <ul class="navbar">
-    <li><a href='/info'>Info</a></li>
-    <li><a href='/setup'>Setup</a></li>
-    <li><a href='/main-ui'>Main UI</a></li>
-    <li><a href='/colour-buttons'>Colour Buttons</a></li>
-    <li><a href='/animations'>Animations</a></li>
-  </ul>
+	<!-- Global Navbar -->
+	<ul class="navbar">
+		<li><a href="/info">Info</a></li>
+		<li><a href="/setup">Setup</a></li>
+		<li><a href="/main-ui">Main UI</a></li>
+		<li><a href="/colour-buttons">Colour Buttons</a></li>
+		<li><a href="/animations">Animations</a></li>
+	</ul>
 
-    <!-- This is where the content of individual pages will go -->
-    <slot/>
+	<!-- This is where the content of individual pages will go -->
+	<slot></slot>
 </main>
 
 <style>
-  /* Global Styles */
-  :global(body){
-    background-color: #2c2c2c;
-    font-family: Arial, Helvetica, sans-serif;
-    color: white;
-    margin: 0;
-    }
+	/* Global Styles */
+	:global(body) {
+		background-color: #2c2c2c;
+		font-family: Arial, Helvetica, sans-serif;
+		color: white;
+		margin: 0;
+	}
 
-  .navbar{
-    display: flex;
-    justify-content: space-around;
-    background-color: #333;
-    padding: 1rem;
-    list-style-type: none;
-  }
-    
-  .navbar li{
-      margin: 0 10px;
-  }
+	.navbar {
+		display: flex;
+		justify-content: space-around;
+		background-color: #333;
+		padding: 1rem;
+		list-style-type: none;
+	}
 
-  .navbar li a {
-    color: white;
-    text-decoration: none;
-  }
+	.navbar li {
+		margin: 0 10px;
+	}
 
-  .navbar li a:hover {
-    color: #ccc;
-  }
+	.navbar li a {
+		color: white;
+		text-decoration: none;
+	}
+
+	.navbar li a:hover {
+		color: #ccc;
+	}
 </style>
-
